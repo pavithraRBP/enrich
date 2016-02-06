9fe62c6834e51100df0fd3dbbcadd82c99a48806
pavithraRBP <pavirbp@gmail.com>
2016-02-06 14:39:26 +0530

    Create araypass.c

---
+++ araypass.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void add(int x[],int y[],int c[])
+{	
+	int i,j,m,n,k;
+	for(i=0;i<3;i++)
+	{
+		c[i]=x[i];
+	}
+	for(m=3,n=0;m<7,n<4;m++,n++)
+	{	
+		c[m]=y[n];
+	}
+}
+void un(int x[],int y[],intc[])
+{
+	int 
+	
+
+main()
+{
+	int c[10],d,e[10],f[10],g[10],a[]={1,3,5},b[]={1,10,5,20},i,j;
+	add(a,b,c);
+	for(i=0;i<7;i++)
+	{
+	printf("%d \n",c[i]);
+	}
+	
+}
