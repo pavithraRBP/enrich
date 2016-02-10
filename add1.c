d863ed5e3959381a80c09fdcef27ff3a4938eca7
pavithraRBP <pavirbp@gmail.com>
2016-02-11 00:04:52 +0530

    Update add1.c

--- add1.c
+++ add1.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+int summer(int a,int b,int c)
+{
+	int add;
+	add= a+b+c;
+	return add;
+}
 int main(void)
 {
-	int a,b,c,sum;
-	scanf("%d%d%d",&a,&b,&c);
+	int a=2,b=3,c=4,sum,k;
 	sum=a+b+c;
-	printf("%d",sum);
-	return 0;
+	printf("%d \n",sum);
+	int p[]={2,3,4},sum1=0,i;
+	for(i=0;i<3;i++)
+	{
+		sum1=p[i]+sum1;
+	}
+	printf("%d \n",sum1);
+	k=summer(a,b,c);
+	printf("%d \n",k);
+ 
 }
