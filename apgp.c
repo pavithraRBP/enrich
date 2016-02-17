8af3f923a6a8fb80a5a15a8e75a0821477cdb92d
pavithraRBP <pavirbp@gmail.com>
2016-02-17 22:49:01 +0530

    Create apgp.c

---
+++ apgp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main(void) {
+	int n,s,d,sum=0,i,res=0;
+	scanf("%d%d%d",&n,&s,&d);
+	sum=s;
+	res=s;
+	printf("%d \t  %d\n",sum,res);
+	for(i=0;i<n;i++)
+	{
+		sum=sum+d;
+  	res=(res*d);
+		printf("%d \t  %d \n",sum,res);
+	}
+	return 0;
+}
